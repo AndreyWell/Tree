a7975bb7a19730d0a415d9a945d0f8ef9e111f01
AndreyWell <andryouwell@gmail.com>
2022-10-05 22:11:11 +0500

    Changed Comparator to lambda

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,36 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
 
-        people.add(new Person("Petya", "Epetrov Lalexin P", 25));
-        people.add(new Person("Alex", "Lalexin", 35));
-        people.add(new Person("Olya", "Golgina-Atanina O", 20));
-        people.add(new Person("Ivan", "Vivanov-Lalexin", 40));
-        people.add(new Person("Tanya", "Atanina-Golgina", 30));
+        people.add(new Person("Petya", "Epetrov Lalexin P", 15));
+        people.add(new Person("Alex", "Lalexin", 25));
+        people.add(new Person("Olya", "Golgina-Atanina O", 12));
+        people.add(new Person("Ivan", "Vivanov-Lalexin", 18));
+        people.add(new Person("Tanya", "Atanina-Golgina T", 17));
         System.out.println(people);
         System.out.println();
-        Collections.sort(people, new SurnameAgeMaxComparator(3));
+        Comparator<Person> SurnameAgeMaxComparator = (o1, o2) -> {
+            int maxWords = 3;
+            int wordsSurname1 = o1.getSurname().split("\\P{IsAlphabetic}+").length;
+            int wordsSurname2 = o2.getSurname().split("\\P{IsAlphabetic}+").length;
+            if (wordsSurname1 >= maxWords && wordsSurname2 >= maxWords) {
+                if (wordsSurname1 > wordsSurname2) {
+                    return 1;
+                } else if (wordsSurname1 < wordsSurname2) {
+                    return -1;
+                } else {
+                    return Integer.compare(o1.getAge(), o2.getAge());
+                }
+            } else {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            }
+        };
+        Collections.sort(people, SurnameAgeMaxComparator);
         System.out.println(people);
     }
 }
