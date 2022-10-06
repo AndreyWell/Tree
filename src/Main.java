0e1c905c72e0038c243e6bbb06f9f45bf68c3eaf
AndreyWell <andryouwell@gmail.com>
2022-10-06 15:12:17 +0500

    Added Predicate<Person>

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,23 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
 
-        people.add(new Person("Petya", "Epetrov Lalexin P", 25));
-        people.add(new Person("Alex", "Lalexin", 35));
-        people.add(new Person("Olya", "Golgina-Atanina O", 20));
-        people.add(new Person("Ivan", "Vivanov-Lalexin", 40));
-        people.add(new Person("Tanya", "Atanina-Golgina", 30));
+        people.add(new Person("Petya", "Epetrov Lalexin P", 15));
+        people.add(new Person("Alex", "Lalexin", 25));
+        people.add(new Person("Olya", "Golgina-Atanina O", 12));
+        people.add(new Person("Ivan", "Vivanov-Lalexin", 18));
+        people.add(new Person("Tanya", "Atanina-Golgina T", 17));
         System.out.println(people);
         System.out.println();
+
+        // Predicate<Person>
+        people.removeIf(person -> person.getAge() < 18);
+
         Collections.sort(people, new SurnameAgeMaxComparator(3));
         System.out.println(people);
     }
